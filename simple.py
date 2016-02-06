996100574fabd75ac9c7364c2e7b7d82e558e3c9
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-02-06 15:08:26 -0500

    Create simple.py
    taken from a youtube video

---
+++ simple.py
@@ -0,0 +1,13 @@
+# simple GUI
+
+from tkinter import *
+
+# create the window
+root = Tk()
+
+# modify the root window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+# kick off event loop
+root.mainloop()
